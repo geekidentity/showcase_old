d90db86d3f989ea9775411cb3817d135402ba81a
侯法超 <houfachao1994@163.com>
2016-07-18 09:48:15 +0800

    modified:   BaseController.java

--- showcase/showcase-api/src/main/java/com/justdoit/showcase/base/controller/BaseController.java
+++ showcase/showcase-api/src/main/java/com/justdoit/showcase/base/controller/BaseController.java
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * 
  */
 package com.justdoit.showcase.base.controller;
@@ -17,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -34,6 +35,8 @@ import com.justdoit.showcase.base.util.JSONUtil;
  *
  * @author 侯法超
  * @date 创建时间：2016年6月20日  下午4:59:32
+ * 
+ * 
  * 
  */
 public abstract class BaseController<T extends BaseEntity<Long>, Service extends BaseService<T, ?>> {
@@ -117,6 +120,10 @@ public abstract class BaseController<T extends BaseEntity<Long>, Service extends
 		
 		if (e instanceof MissingServletRequestParameterException) {
 			handleMissingServletRequestParameterException(request, response, handlerMethod, e);
+		} else if (e instanceof AccessDeniedException) {
+			handleAccessDeniedException(request, response, handlerMethod, e);
+		} else {
+			handleUnknowException(request, response, handlerMethod, e);
 		}
 		
 	}
@@ -124,24 +131,50 @@ public abstract class BaseController<T extends BaseEntity<Long>, Service extends
 	private void handleMissingServletRequestParameterException(HttpServletRequest request,
 			HttpServletResponse response, HandlerMethod handlerMethod, Exception e) {
 		MissingServletRequestParameterException me = (MissingServletRequestParameterException) e;
-		
+		responseError(-1, "required parameters:" + me.getParameterName(), 400, request, response);
+	}
+	
+	/**
+	 * 处理未授权的异常
+	 * @param request
+	 * @param response
+	 * @param handlerMethod
+	 * @param e
+	 */
+	private void handleAccessDeniedException(HttpServletRequest request,
+			HttpServletResponse response, HandlerMethod handlerMethod, Exception e) {
+		AccessDeniedException exception = (AccessDeniedException) e;
+		responseError(403, exception.getMessage(), 403, request, response);
+	}
+	
+	private void handleUnknowException(HttpServletRequest request,
+			HttpServletResponse response, HandlerMethod handlerMethod, Exception e) {
+		responseError(500, e.getMessage(), 500, request, response);
+	}
+	
+	/**
+	 * 向客户端响应异常消息
+	 * @param code 异常编号
+	 * @param msg 异常信息
+	 * @param status HTTP响应头
+	 * @param request
+	 * @param response
+	 */
+	public static void responseError(int code, String msg, int status, HttpServletRequest request, HttpServletResponse response) {
 		response.setContentType("application/json;charest=UTF-8");
-		response.setStatus(400);
+		response.setStatus(status);
 		PrintWriter out = null;
-		
-		
 		try {
 			out = response.getWriter();
-			Map<Object, Object> msg = new HashMap<>();
-			msg.put("code", "required parameters:" + me.getParameterName());
-			msg.put("error_code", -1);
-			JSONUtil.map2Json(out, msg);
+			Map<Object, Object> info = new HashMap<>();
+			info.put("code", code);
+			info.put("msg", msg);
+			JSONUtil.map2Json(out, info);
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		} finally {
 			out.close();
 		}
-		
 	}
 }
