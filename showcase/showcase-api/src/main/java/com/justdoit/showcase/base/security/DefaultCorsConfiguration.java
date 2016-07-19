c92fc6ba906bde9e8513d66c1ee2af658d0c0e80
侯法超 <houfachao1994@163.com>
2016-07-19 15:37:19 +0800

    cors

---
+++ showcase/showcase-api/src/main/java/com/justdoit/showcase/base/security/DefaultCorsConfiguration.java
@@ -0,0 +1,21 @@
+package com.justdoit.showcase.base.security;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Component
+public class DefaultCorsConfiguration  implements CorsConfigurationSource  {
+
+	@Override
+	public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedMethod("*");
+		configuration.addAllowedOrigin("*");
+		return null;
+	}
+	
+}
